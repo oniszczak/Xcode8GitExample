c430143b90506aacf94c567c1908c45b9de9af56
ALEKSANDER ONISZCZAK <aleks@aleksanders-macbook-pro.local>
2017-05-11 14:50:02 -0400

    Initial Commit

---
+++ GitHubTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitHubTest
+//
+//  Created by ALEKSANDER ONISZCZAK on 2017-05-11.
+//  Copyright © 2017 ALEKSANDER ONISZCZAK. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by ALEKSANDER ONISZCZAK on 2017-05-11.
+//  Copyright © 2017 ALEKSANDER ONISZCZAK. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
